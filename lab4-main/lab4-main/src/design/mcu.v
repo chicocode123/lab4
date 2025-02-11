d2fa42f9bdebccee39f11157cf8e08fc209ad2f0
rmabdullah38 <117119500+rmabdullah38@users.noreply.github.com>
2025-02-11 14:08:06 -0800

    Working MCU

--- lab4-main/lab4-main/src/design/mcu.v
+++ lab4-main/lab4-main/src/design/mcu.v
@@ -1,14 +1,60 @@
+`define PAUSE 2'b00
+`define PLAY 2'b01
+`define NEXT 2'b10
+
 module mcu(
     input clk,
     input reset,
     input play_button,
     input next_button,
-    output play,
-    output reset_player,
-    output [1:0] song,
+    output reg play,
+    output reg reset_player,
+    output reg [1:0] song,
     input song_done
 );
 
-    // Implementation goes here!
+    wire [1:0 ]current_state;
+    reg [1:0] next_state;    
+    
+    dffr #(2) state_reg (
+        .clk(clk),
+        .r(reset),
+        .d(next_state),
+        .q(current_state)
+    );
+    always @(*) begin
+        if (reset) begin
+            song = 2'b00;
+        end
+    end
+    always @(*) begin
+        reset_player = (next_button || song_done);
 
+        if (play_button) begin
+            next_state = (current_state == `PAUSE || `NEXT) ? `PLAY : `PAUSE;
+        end else if (next_button || song_done) begin
+            next_state = `NEXT;
+        end else begin
+            next_state = current_state;
+        end
+    end
+    
+    always @(*) begin
+        case (current_state)
+            `PLAY : begin
+                play = 1;
+            end
+            `PAUSE : begin
+                play = 0;
+            end
+            `NEXT : begin
+                song = song + 1;
+                play = 0;
+                next_state = `PAUSE;
+            end
+            default : begin
+                play = 0;
+            end
+        endcase
+    end
 endmodule
