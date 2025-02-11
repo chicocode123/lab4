f0d1bfb307111121e6eece8e13720ef974afac07
rmabdullah38 <117119500+rmabdullah38@users.noreply.github.com>
2025-02-11 14:09:50 -0800

    Updated mcu_tb.v - should be good

--- lab4-main/lab4-main/src/sim/mcu_tb.v
+++ lab4-main/lab4-main/src/sim/mcu_tb.v
@@ -23,9 +23,47 @@ module mcu_tb();
         forever #5 clk = ~clk;
     end
 
-    // Tests
+    
     initial begin
-
+    next_button = 0; // Initializing everything to 0
+    play_button = 0;
+    song_done = 0;
+    
+    #20
+    play_button = 1; // Playing song 1
+    #10 play_button = 0;
+    
+    #405
+    next_button = 1; // Next to song 2
+    #10 next_button = 0;
+    
+    #100
+    play_button = 1; // Playing song 2
+    #10 play_button = 0;
+    
+    #400
+    song_done = 1; // Song 2 Finishes so move to song 3
+    #10 song_done = 0;
+    
+    #100
+    play_button = 1; // Play song 3
+    #10 play_button = 0;
+    
+    #400
+    song_done = 1; // Song 3 is done move to song 4
+    #10 song_done = 0;
+    
+    #100
+    play_button = 1; // play song 4
+    #10 play_button = 0;
+    
+    #400
+    song_done = 1; // song 4 done, move back to start
+    #10 song_done = 0;
+    
+    #100
+    play_button = 1; // playing (should be song 1)
+    #10 play_button = 0;
     end
 
 endmodule
