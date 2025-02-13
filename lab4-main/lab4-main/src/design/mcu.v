f117486c0761a474a530b2df154669155e8d441c
rmabdullah38 <117119500+rmabdullah38@users.noreply.github.com>
2025-02-12 20:59:55 -0800

    Update mcu.v FINAL

--- lab4-main/lab4-main/src/design/mcu.v
+++ lab4-main/lab4-main/src/design/mcu.v
@@ -12,8 +12,7 @@ module mcu(
     output [1:0] song,
     input song_done
 );
-    reg play_temp;
-    reg reset_player_temp;
+    reg play_temp, reset_player_temp;
     reg [1:0] song_temp;
     wire [1:0] current_state;
     reg [1:0] next_state;    
@@ -24,29 +23,41 @@ module mcu(
         .d(next_state),
         .q(current_state)
     );
-
+    always @(*) begin
+        if (reset) begin
+            song_temp = 2'b00;
+        end
+    end
     always @(*) begin
         reset_player_temp = (next_button || song_done);
 
-        if (reset) begin
-            song_temp = 2'b00;
-        end else if (play_button) begin
+        if (play_button) begin
             next_state = (current_state == `PAUSE || `NEXT) ? `PLAY : `PAUSE;
         end else if (next_button || song_done) begin
             next_state = `NEXT;
-        end else if (current_state == `PLAY) begin
-            play_temp = 1;
-        end else if (current_state == `PAUSE) begin
-            play_temp = 0;
-        end else if (current_state == `NEXT) begin
-            song_temp = song_temp + 1;
-            play_temp = 0;
         end else begin
             next_state = current_state;
         end
     end
-        
+    
+    always @(*) begin
+        case (current_state)
+            `PLAY : begin
+                play_temp = 1;
+            end
+            `PAUSE : begin
+                play_temp = 0;
+            end
+            `NEXT : begin
+                song_temp = song_temp + 1;
+                play_temp = 0;
+            end
+            default : begin
+                play_temp = 0;
+            end
+        endcase
+    end
     assign play = play_temp;
-    assign reset_player = reset_player_temp;
     assign song = song_temp;
+    assign reset_player = reset_player_temp;
 endmodule
